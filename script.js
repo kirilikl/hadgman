e5bb5499db0716e14809c3969dfa25fbeb972d47
kirilikl <kirill.ahremenko@gmail.com>
2024-04-14 00:09:04 +0300

    version 0.1

---
+++ script.js
@@ -0,0 +1,30 @@
+let playBut = document.querySelector(".play")
+let keyboard = document.querySelector(".keyboard")
+let game = document.querySelector(".game")
+let word = document.querySelector(".word")
+
+let currentWord = "ПОСОСЕШЬОК"
+
+function checkLetter(button, clickedLetter) {
+	if (currenWord.includes(clickedLetter)) {
+		if (letter === clickedLetter) {
+		}
+	}
+}
+
+playBut.addEventListener("click", () => {
+	for (let i = 1072; i <= 1103; i++) {
+		let button = document.createElement(`button`)
+		button.innerHTML = String.fromCharCode(i)
+		keyboard.appendChild(button)
+		button.addEventListener("click", (e) =>
+			initGame(e.target, String.fromCharCode(i))
+		)
+	}
+
+	for (let i = 0; i < currentWord.length; i++) {
+		word.innerHTML += "_"
+	}
+	playBut.style.display = "none"
+	game.style.display = "flex"
+})
